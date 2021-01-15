bcac30e27bca69148075d4503a0a81549ceb13cc
Gabi Mesquita <70117817+gabimesquita@users.noreply.github.com>
2021-01-15 18:47:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var data = new Date()
 var hora = data.getHours()
 var min = data.getMinutes()
 
-if(hora>=0 && hora<12){
+if(hora>=0 && hora<11){
     msg.innerHTML = `Bom dia! Agora são ${hora}:${min}.`
     img.src="manha.png"    
     document.body.style.background='#e2cd9f'
-    }else if(hora>=12 && hora<=18){
+    }else if(hora>=11 && hora<=17){
     msg.innerHTML = `Boa tarde! Agora são ${hora}:${min}.`
     img.src="tarde.jpg"
     document.body.style.background='#b9846f'
